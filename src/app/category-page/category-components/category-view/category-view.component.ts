8c9921b6fa5b8f2ee731349413bb77947bddc44e
Sphah <sbuthelezi2@ulwembubs.com>
2023-12-04 12:33:06 +0200

    subscribing to actions and binding results to table.
    navigating to  details on item clicked in the table.
    unsubscribing to subscription when component is destroyed

--- src/app/category-page/category-components/category-view/category-view.component.ts
+++ src/app/category-page/category-components/category-view/category-view.component.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CategoryState } from '../../category-state-manager/category.state';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subscription } from 'rxjs';
 import { Category } from '../../model/category';
-import { GetCategory } from '../../category-state-manager/category.actions';
+import { GetCategory, GetCategoryDetails } from '../../category-state-manager/category.actions';
+import { MatTableDataSource } from '@angular/material/table';
+import { CategoryGetService } from '../../category-state-manager/service/category-get.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-view',
   templateUrl: './category-view.component.html',
   styleUrls: ['./category-view.component.scss']
 })
-export class CategoryViewComponent {
- // @Select(CategoryState.getAllCategories) categories$! : Observable<Category[]>
-sub : Subscription = new Subscription();
-cat$ : Observable<Category[]> = new Observable<Category[]>
+export class CategoryViewComponent implements OnInit,OnDestroy{
+sub! : Subscription;
+public dataSource!: MatTableDataSource<Category>;
+displayedColumns: string [] = ['Code','Name'];
 
-constructor(private store: Store){}
+constructor(private store: Store,
+            private router: Router){}
+
   ngOnInit(){
-    //this.sub = this.cat$.subscribe(data=> data = this.store.dispatch(new GetCategory()))
-    this.cat$ = this.store.dispatch(new GetCategory())
-    console.log(this.store.dispatch(new GetCategory()))
+    this.store.dispatch(new GetCategory());
+    this.sub = this.store.select(CategoryState.getAllCategories).subscribe(data =>{
+      this.dataSource = new MatTableDataSource(data);
+    });
   }
 
+  viewDetails(category: Category){
+    this.store.dispatch(new GetCategoryDetails(category));
+    this.router.navigate(['category/detail']);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+  }
 }
