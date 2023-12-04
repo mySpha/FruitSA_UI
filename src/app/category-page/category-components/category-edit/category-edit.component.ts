7c848573c34ba0e59e76cbaa0d2c3d9a9da9b9a4
Sphah <sbuthelezi2@ulwembubs.com>
2023-12-04 12:35:46 +0200

    binding data to edit view

--- src/app/category-page/category-components/category-edit/category-edit.component.ts
+++ src/app/category-page/category-components/category-edit/category-edit.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { CategoryState } from '../../category-state-manager/category.state';
+import { Category } from '../../model/category';
 
 @Component({
   selector: 'app-category-edit',
   templateUrl: './category-edit.component.html',
   styleUrls: ['./category-edit.component.scss']
 })
-export class CategoryEditComponent {
+export class CategoryEditComponent implements OnInit{
+  @Output() submitEM = new EventEmitter();
+  public editCategoryForm!: FormGroup;
+  category!: Category | null
+  isValidOptions : any[] = [{'value':true, display:'true'},
+                            {'value':false, display:'false'}]
+  selected: boolean | undefined;
+  @Output() submit = new EventEmitter()
+  constructor(private fb: FormBuilder,
+              private store: Store,
+              private router: Router){}
+  
+  ngOnInit(): void {    
+    this.store.select(CategoryState.getCategoryDetails).subscribe(data =>{
+    this.category = data
+  this.selected = data?.isActive});
+    this.initializeForm();
+  }
 
+  initializeForm(){
+    this.editCategoryForm = this.fb.group({
+      'name': new FormControl(this.category?.name, Validators.required ),
+      'categoryCode': new FormControl(this.category?.categoryCode, Validators.required),
+      'isActive': new FormControl(this.selected, Validators.required)
+    })
+  }
+  selectionChange(value: any){
+    this.selected = value;
+  }
+  onSubmit(loginForm : FormGroup){
+    if (loginForm.valid) {
+      console.log(loginForm.value);
+    }
+  }
 }
