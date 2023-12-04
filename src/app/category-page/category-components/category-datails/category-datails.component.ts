c897bfaa992573e5743a2afd31b0c9a4ffe6d865
Sphah <sbuthelezi2@ulwembubs.com>
2023-12-04 12:34:59 +0200

    binding data to detail view

--- src/app/category-page/category-components/category-datails/category-datails.component.ts
+++ src/app/category-page/category-components/category-datails/category-datails.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { CategoryState } from '../../category-state-manager/category.state';
+import { Observable } from 'rxjs';
+import { Category } from '../../model/category';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-datails',
   templateUrl: './category-datails.component.html',
   styleUrls: ['./category-datails.component.scss']
 })
-export class CategoryDatailsComponent {
+export class CategoryDatailsComponent implements OnInit{
 
+  @Select(CategoryState.getCategoryDetails) detail$!: Observable<Category>;
+  detail!: Category | null
+  constructor(private store: Store,
+    private router: Router){}
+
+    ngOnInit(){
+    this.store.select(CategoryState.getCategoryDetails).subscribe(data =>{
+    this.detail = data});
 }
+
+goToEdit(){
+  this.router.navigate(['category/edit'])
+}
+}
+
+
