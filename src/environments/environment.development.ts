0bf18dfc7531cf12ecf444996e6f6aae26dc497e
Sphah <sbuthelezi2@ulwembubs.com>
2023-12-04 12:26:12 +0200

    updating endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,3 +2,4 @@ export const environment = {
     
 };
 export const BaseUrl = '../assets/mock-data/category-data.json'
+export const AuthUrl = '../assets/mock-data/users.json'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,3 +3,4 @@ export const environment = {
 };
  //for prod, update BaseUrl
 export const BaseUrl = '../assets/mock-data/category-data.json'
+export const AuthUrl = '../assets/mock-data/users.json'
